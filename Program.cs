a3f8b82a918deadebb549496b425ee350b694a1c
Dylan00Platas <131178283+Dylan00Platas@users.noreply.github.com>
2024-03-11 08:21:25 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AppSinHilos;
+ 
+class Program
+{
+ 
+    public static void DoTrabajoPesado()
+    {
+        Console.WriteLine("DoTrabajoPesado: Estoy levantando un elefante!");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: Cansado! Necesito una siesta de 3 seg");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 1...");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 2...");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 3...");
+        Console.WriteLine("DoTrabajoPesado: Ya desperté");
+    }
+ 
+    public static void DoAlgo()
+    {
+        Console.WriteLine("DoAlgo: holaaaa! Haciendo algo  por aquí");
+ 
+        for (int i = 0; i < 20; i++)
+            Console.Write($"{i}" );
+        Console.WriteLine();
+        Console.WriteLine("DoAlgo: Ya terminé");
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Dylan Platas: Desarrollo de sistemas en red \n");
+ 
+        Console.WriteLine("El main thread comienza aquí:");
+        Program.DoTrabajoPesado();
+        Program.DoAlgo();
+ 
+        Console.WriteLine("\n El main thread termina aquí");
+        Console.WriteLine("\n Presione cualquier tecla para terminar...");
+        Console.ReadKey(true);
+    }
+}
